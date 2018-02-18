bf098be99999aaa0bcba175731201efc30407b7a
Lucas Stanek <staneklu@msu.edu>
2018-02-18 15:16:01 -0500

    Added particles for momenta mirrors

--- output.py
+++ output.py
@@ -6,7 +6,7 @@ Module for output - preparing the data into format suitable for visualization or
 import numpy as np
 from numba import jit
 
-def write_pos_vel_hist(filename, pos, mom, KE):
+def write_pos_vel_hist(filename, pos, mom, KE, pis, Lx, Ly, Lz):
    '''
    Write positions and velocities into given file.
    
@@ -22,15 +22,30 @@ def write_pos_vel_hist(filename, pos, mom, KE):
    '''
    Msteps = len(pos)
    N = len(pos[0])
-
+   tpos = np.zeros((8,3))
+ 
    fp = open(filename, "wb")
    
    print(np.shape(pos[0]), np.shape(mom[0]), np.shape(KE[0]))
    for i in range(Msteps):
+	
+      # pistion postion
+      tpos[0] = [0,0,pis[i]]
+      tpos[1] = [0,Ly,pis[i]]
+      tpos[2] = [Lx,Ly,pis[i]]
+      tpos[3] = [Lx,0,pis[i]]
+	  
+      # right boundary
+      tpos[4] = [0,0,Lz]
+      tpos[5] = [0,Ly,Lz]
+      tpos[6] = [Lx,Ly,Lz]
+      tpos[7] = [Lx,0,Lz]
       # write current step to file
       np.savetxt(fp, np.c_[pos[i], mom[i], KE[i]], 
-                 fmt="%f %f %f %f %f %f %f", header=str(N)+"\n"+ "x y z vx vy vz KE", comments = "")
+                 fmt="%f %f %f %f %f %f %f", header=str(N+8)+"\n"+ "x y z vx vy vz KE", comments = "")
    
+      np.savetxt(fp, np.c_[tpos, np.zeros((8,3)), np.zeros(8)], 
+                 fmt="%f %f %f %f %f %f %f", header="", comments = "")
    fp.close()
    print("Positions and momenta saves into file: {0}".format(filename))
 
